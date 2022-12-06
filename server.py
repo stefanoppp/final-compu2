b6c589fc2207c50baf61f4ea32a86122dfae446c
Stefano <49915584+stefanoppp@users.noreply.github.com>
2022-12-06 12:41:04 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,44 +1,44 @@
 import socket
 import threading
+def main():
+    HEADER=64
+    PORT=5050
+    FORMAT='utf-8'
+    DISCONNECT_MESSAGE="quit"
+    SERVER=socket.gethostbyname(socket.gethostname())
+    ADDR=(SERVER,PORT)
 
 
-# cabecera de los mensajes que seran enviados en bytes
-HEADER=64
-PORT=5050
-FORMAT='utf-8'
-DISCONNECT_MESSAGE="quit"
-SERVER=socket.gethostbyname(socket.gethostname())
-ADDR=(SERVER,PORT)
+    server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+    server.bind(ADDR)
 
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# creamos server
-server.bind(ADDR)
-
-# metodo para manejar las comunicaciones entre cliente y servidor. Se ejecuta
-# una vez por cada cliente
-def handle_client(conn,addr):
-    connected=True
-    print(f"Nuevo cliente conectado. Direccion {addr}")
-    while connected:
-        msg_length=conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            
-            msg_length=int(msg_length)
-            msg=conn.recv(msg_length).decode(FORMAT)
-            if msg==DISCONNECT_MESSAGE:
-                connected=False
-            print(f"Usuario {addr} dice {msg}")
-            conn.send(f"(SERVER MESSAGE). Usted dijo {msg}".encode(FORMAT))
-    conn.close()    
-    
-def start():
-    print("Servidor escuchando...")
-    server.listen()
-    while True:
-        # cuando hay una nueva conexin, creamos un thread con la funcion handle client
-        conn, addr=server.accept()
-        thread=threading.Thread(target=handle_client,args=(conn,addr))
-        thread.start()
+    def handle_client(conn,addr):
+        connected=True
+        print(f"Nuevo cliente conectado. Direccion {addr}")
+        while connected:
+            msg_length=conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                
+                msg_length=int(msg_length)
+                msg=conn.recv(msg_length).decode(FORMAT)
+                if msg==DISCONNECT_MESSAGE:
+                    connected=False
+                # imprimo en la consola del server
+                print(f"Usuario {addr} dice {msg}")
+                # imprimo en la consola del client
+                conn.send(f"(SERVER MESSAGE). Usted dijo {msg}".encode(FORMAT))
+        conn.close()    
         
-start()
+    def start():
+        print("Servidor escuchando...")
+        server.listen()
+        
+        while True:
+            # cuando hay una nueva conexin, creamos un thread con la funcion handle client
+            conn, addr=server.accept()
+            thread=threading.Thread(target=handle_client,args=(conn,addr))
+            thread.start()
+            
+    start()
+main()
